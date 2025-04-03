ee975cefe2445169e491c8076d0a7f84da1a7258
Erik Pizana <erikpzcanedo@outlook.com>
2025-04-03 16:34:50 -0600

    Listeners

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,33 @@ import './App.css'
 
 function App() {
 
+  const [message, setmessage] = useState(null);
+  const [message2, setmessage2] = useState(null);
+
+  const listenEvent = (event) => {
+    console.log("Received event.data from listener 'message'", event.data);
+    setmessage(event.data);
+  }
+  
   useEffect(()  => {
-    window.addEventListener('message', (event) => {
-      console.log("Received message", event.data.action);
-      if (event.data.action === 'pause') {
-          console.log("Pause");
-      } else if (event.data.action === 'play') {
-         console.log("Play");
-      } else if (event.data.action === 'next') {
-          console.log("Next");
-      } else if (event.data.action === 'repeat') {
-          console.log("Repeat");  
-      }
-  });
+    window.addEventListener('message', listenEvent);
+    return () => {
+      window.removeEventListener('message', listenEvent);
+    }
   }, [])
+  
+  useEffect(() => {
+    window.receiveMessageFromNative = (msg) => {
+      console.log("Received from 'receiveMessageFromNative'", msg);
+      setmessage2(msg);
+    };
+  }, []);
 
   return (
     <div>
       POC-CMW
+      <h3>Message A: {message}</h3>
+      <h3>Message B: {message2}</h3>
     </div>
   )
 }
